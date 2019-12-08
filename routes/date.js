300451d2fa72f9738338163234a3c1ddc3e5c9be
Tyler Hammer <tyler@tylerhammer.com>
2019-12-08 13:31:54 -0500

    Refactored Date Route
    Created additional functionality to improve dynamicness

--- routes/date.js
+++ routes/date.js
@@ -3,39 +3,91 @@ const router = express.Router();
 const moment = require('moment');
 const fs = require('fs');
 
+var currentRecord;
+var prevRecord;
+var bestRecord;
+var origDate;
+
 router.get('/', (req, res) => {
-  fs.readFile('date.json', (err, data) => {
-    if (err) {
+  loadFile('data.json')
+    .then(() => {
+      let recordInfo = {
+        currentRecord: currentRecord,
+        bestRecord: bestRecord,
+        prevRecord: prevRecord
+      };
+      console.log(recordInfo);
+      res.status(200).send(recordInfo);
+    })
+    .catch(err => {
       res.status(500).send(err);
-    } else {
-      let jsonObject = JSON.parse(data);
-      let origDate = moment(jsonObject.origDate, 'YYYY-MM-DD');
-      let currentDate = moment();
-      //let diff = moment.duration(currentDate.diff(origDate)).asDays();
-      let diff = currentDate.diff(origDate, 'days');
-      res.status(200).send(`${diff}`);
-    }
-  });
+    });
 });
 
 router.get('/reset', (req, res) => {
-  let origDate = {
-    origDate: moment()
-  };
+  loadFile('data.json')
+    .then(() => {
+      newObject = {
+        origDate: moment(),
+        prevRecord: currentRecord,
+        bestRecord: bestRecord
+      };
 
-  let data = JSON.stringify(origDate);
-  fs.writeFileSync('date.json', data);
-
-  res.status(200).send(data);
+      let newData = JSON.stringify(newObject);
+      fs.writeFileSync('data.json', newData);
+      res.status(200).send(newData);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
 });
 
 router.get('/initialize', (req, res) => {
   let origDate = {
-    origDate: moment('2019-12-07', 'YYYY-MM-DD')
+    origDate: moment('2019-12-08', 'YYYY-MM-DD'),
+    prevRecord: 71,
+    bestRecord: 71
   };
   let data = JSON.stringify(origDate);
-  fs.writeFileSync('date.json', data);
+  fs.writeFileSync('data.json', data);
   res.status(200).send(data);
 });
+
+function checkRecord(jsonObject, currentRecord) {
+  let prevRecord = jsonObject.prevRecord;
+  let bestRecord = jsonObject.bestRecord;
+  let data = {};
+
+  if (currentRecord > bestRecord) {
+    data.origDate = jsonObject.origDate;
+    data.bestRecord = currentRecord;
+    data.prevRecord = prevRecord;
+    console.log('bestRecord updated');
+    let newData = JSON.stringify(data);
+    fs.writeFileSync('data.json', newData);
+
+    return data.bestRecord;
+  } else {
+    return bestRecord;
+  }
+}
+
+async function loadFile(fileName) {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(fileName, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let currentDate = moment();
+        jsonObject = JSON.parse(data);
+        origDate = moment(jsonObject.origDate, 'YYYY-MM-DD');
+        currentRecord = currentDate.diff(origDate, 'days');
+        bestRecord = checkRecord(jsonObject, currentRecord);
+        prevRecord = jsonObject.prevRecord;
+        resolve();
+      }
+    });
+  });
+}
 
 module.exports = router;
